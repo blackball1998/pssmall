747e5e6fc648c6155454f2bdcfaa39ce2534e706
blackball1998 <balckball1998@outlook.com>
2021-01-09 17:21:41 +0800

    add category

--- pssmall-product/src/main/java/com/blackball/pssmall/product/controller/CategoryController.java
+++ pssmall-product/src/main/java/com/blackball/pssmall/product/controller/CategoryController.java
@@ -1,20 +1,16 @@
 package com.blackball.pssmall.product.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
+import com.blackball.common.utils.R;
+import com.blackball.pssmall.product.entity.CategoryEntity;
+import com.blackball.pssmall.product.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.blackball.pssmall.product.entity.CategoryEntity;
-import com.blackball.pssmall.product.service.CategoryService;
-import com.blackball.common.utils.PageUtils;
-import com.blackball.common.utils.R;
-
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -30,15 +26,11 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    /**
-     * 列表
-     */
     @RequestMapping("/list")
     //@RequiresPermissions("product:category:list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
-
-        return R.ok().put("page", page);
+    public R list() {
+        List<CategoryEntity> list = categoryService.listAsTree();
+        return R.ok().put("data", list);
     }
 
 
@@ -47,8 +39,8 @@ public class CategoryController {
      */
     @RequestMapping("/info/{catId}")
     //@RequiresPermissions("product:category:info")
-    public R info(@PathVariable("catId") Long catId){
-		CategoryEntity category = categoryService.getById(catId);
+    public R info(@PathVariable("catId") Long catId) {
+        CategoryEntity category = categoryService.getById(catId);
 
         return R.ok().put("category", category);
     }
@@ -58,8 +50,8 @@ public class CategoryController {
      */
     @RequestMapping("/save")
     //@RequiresPermissions("product:category:save")
-    public R save(@RequestBody CategoryEntity category){
-		categoryService.save(category);
+    public R save(@RequestBody CategoryEntity category) {
+        categoryService.save(category);
 
         return R.ok();
     }
@@ -69,8 +61,8 @@ public class CategoryController {
      */
     @RequestMapping("/update")
     //@RequiresPermissions("product:category:update")
-    public R update(@RequestBody CategoryEntity category){
-		categoryService.updateById(category);
+    public R update(@RequestBody CategoryEntity category) {
+        categoryService.updateById(category);
 
         return R.ok();
     }
@@ -80,8 +72,8 @@ public class CategoryController {
      */
     @RequestMapping("/delete")
     //@RequiresPermissions("product:category:delete")
-    public R delete(@RequestBody Long[] catIds){
-		categoryService.removeByIds(Arrays.asList(catIds));
+    public R delete(@RequestBody Long[] catIds) {
+        categoryService.removeByIds(Arrays.asList(catIds));
 
         return R.ok();
     }
--- pssmall-product/src/main/java/com/blackball/pssmall/product/service/impl/CategoryServiceImpl.java
+++ pssmall-product/src/main/java/com/blackball/pssmall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.blackball.pssmall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +29,24 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listAsTree() {
+        List<CategoryEntity> categoryList = baseMapper.selectList(null);
+        return categoryList.stream()
+                .filter(menu -> menu.getParentCid() == 0)
+                .peek(menu -> menu.setChildren(getSubmenu(menu, categoryList)))
+                .sorted(Comparator.comparingInt(CategoryEntity::getSort))
+                .collect(Collectors.toList());
+    }
+
+    private List<CategoryEntity> getSubmenu(CategoryEntity root, List<CategoryEntity> categoryList) {
+        return categoryList.stream()
+                .filter(menu -> menu.getParentCid().equals(root.getCatId()))
+                .peek(menu -> menu.setChildren(getSubmenu(menu, categoryList)))
+                .sorted(Comparator.comparingInt(CategoryEntity::getSort))
+                .collect(Collectors.toList());
     }
 
 }
